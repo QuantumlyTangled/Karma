2e5094c2d1bc888b2d1dc89edcf85bd5dd6cabb4
QuantumlyTangled <EthoArx@gmail.com>
2019-05-04 22:01:07 +0200

    Tyler HELP PLEASE

--- Karma/API/Reddit/BaseFetcher.cs
+++ Karma/API/Reddit/BaseFetcher.cs
@@ -1,7 +1,30 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Web;
+
 namespace Karma.API.Reddit
 {
     public class BaseFetcher
     {
         private readonly string _sub;
+
+        public BaseFetcher(string subReddit)
+        {
+            _sub = subReddit;
+        }
+        
+        public Stream FetchAllPosts()
+        {
+            Stream stream;
+            var builder = new UriBuilder($"https://www.reddit.com/r/{_sub}.json");
+            var query = HttpUtility.ParseQueryString(builder.Query);
+            query["sort"] = "top";
+            query["t"] = "week";
+            query["limit"] = "800";
+            builder.Query = query.ToString();
+            using (var client = new HttpClient()) { stream = client.GetStreamAsync(builder.ToString()).GetAwaiter().GetResult(); }
+            return stream;
+        }
     }
 }
